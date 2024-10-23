95b4db96ae43e7a4b842e24b43b2952286d1f1c6
Hiep Le <hile@strongtie.com>
2024-10-23 17:19:40 +0700

    Add router and root layout

---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+function Home() {
+  return (
+    <div className="card">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={"/vite.svg"} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img
+            src={"/src/assets/react.svg"}
+            className="logo react"
+            alt="React logo"
+          />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default Home;
