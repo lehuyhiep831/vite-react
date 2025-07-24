61eda2c2fd475fb78a646eea13cf0013ca8c0a57
Hiep Huy Le <hile@strongtie.com>
2025-07-24 12:05:42 +0700

    Divide List into Sections (#issue3) feat: add SectionConfirm component to display created sections and integrate it into Mandarin page Fixes #10

---
+++ src/features/mandarin/components/SectionConfirm.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+type Section = {
+  sectionId: string;
+  wordIds: (string | number)[];
+};
+
+type Props = {
+  sections: Section[];
+  wordsPerSection: number;
+  onProceed: () => void;
+};
+
+export function SectionConfirm({
+  sections,
+  wordsPerSection,
+  onProceed,
+}: Props) {
+  return (
+    <div style={{ textAlign: "center", marginTop: 32 }}>
+      <h2>Sections Created</h2>
+      <p>
+        {sections.length} sections created, {wordsPerSection} words per section
+        {sections.length > 0 &&
+        sections[sections.length - 1].wordIds.length !== wordsPerSection
+          ? ` (last section: ${
+              sections[sections.length - 1].wordIds.length
+            } words)`
+          : ""}
+      </p>
+      <button onClick={onProceed} style={{ marginTop: 16 }}>
+        Proceed
+      </button>
+    </div>
+  );
+}
